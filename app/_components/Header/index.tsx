97ee02fce65d8f9ec0b9f8db7c8ea1e70c74f0f4
suzu-shika <msz.006.ts@gmail.com>
2026-07-14 19:13:30 +0900

    4章まで完了

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./index.module.css";
+import Menu from "../Menu";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <a href="/" className={styles.logoLink}>
+      <Link href="/" className={styles.logoLink}>
         <Image
           src="/logo.svg"
           alt="SIMPLE"
@@ -14,7 +16,8 @@ export default function Header() {
           priority
           loading="eager"
         />
-      </a>
+      </Link>
+      <Menu />
     </header>
   );
 }
--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./index.module.css";
 import Category from "../Category";
 import Date from "../Date";
@@ -16,7 +17,7 @@ export default function NewsList({ news }: Props) {
     <ul>
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
-          <div className={styles.link}>
+          <Link href={`/news/${article.id}`} className={styles.link}>
             <Image
               className={styles.image}
               src="/no-image.png"
@@ -31,7 +32,7 @@ export default function NewsList({ news }: Props) {
                 <Date date={article.publishedAt ?? article.createdAt} />
               </dd>
             </dl>
-          </div>
+          </Link>
         </li>
       ))}
     </ul>
